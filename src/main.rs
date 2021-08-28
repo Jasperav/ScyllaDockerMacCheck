576545e1a88b32d6a795c2fe1dd3f2fc068cf01f
Jasper <Jasperav@hotmail.com>
2021-08-28 11:19:48 +0200

    commit

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,11 @@ async fn main() {
 
     assert!(result.success(), "{:#?}", result);
 
-    tokio::time::sleep(tokio::time::Duration::from_secs(100)).await;
+    tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
 
-    let result = Command::new("docker")
-        .args(&["exec", "b-scylla", "nodetool", "status"])
-        .status()
+    let result = Command::new("curl")
+        .args(&["-X", "GET", "localhost:9200/_cat/nodes?v=true&pretty"])
+        .output()
         .unwrap();
 
     panic!("{:#?}", result);
